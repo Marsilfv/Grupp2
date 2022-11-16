82e901dc7528fc23f8b2f6bb76b68f943a7e27e9
angelic <angelic97.andersson@gmail.com>
2022-11-16 15:47:03 +0100

    Final Changes, added footer to index html page.

--- assets/js/headerColorChange.js
+++ assets/js/headerColorChange.js
@@ -4,11 +4,9 @@ var scrollTrigger = 60;
 window.onscroll = function() {
   // added a pageYOffset for compatibility with Internet Explorer
   if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
-    console.log('here');
     document.getElementsByTagName("header")[0].classList.add(className);
   } else {
     document.getElementsByTagName("header")[0].classList.remove(className);
-    console.log('heredfdsf');
   }
 };
    
